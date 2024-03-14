638b5d28748a4be0471b96d42dc809696f3f6c90
SathyaPrakash T <78905808+sathya008@users.noreply.github.com>
2024-03-14 23:13:16 +0530

    Update contact.js

--- forms/contact.js
+++ forms/contact.js
@@ -1,26 +1,21 @@
-// api/contact.js
+// forms/contact.js
 
-const { parse } = require('url');
 const { exec } = require('child_process');
 
 module.exports = async (req, res) => {
-  const { pathname, query } = parse(req.url, true);
-  
-  if (pathname === '/forms/contact.php') {
-    // Run the PHP script using the child_process module
-    exec('php forms/contact.php', (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
-      res.end(stdout);
-    });
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found');
-  }
+  // Execute the PHP script
+  exec('php forms/contact.php', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Internal Server Error');
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(stdout);
+  });
 };
