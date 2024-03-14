f9891c5771afa1dcdf191f97a4f89e51a36ec225
SathyaPrakash T <78905808+sathya008@users.noreply.github.com>
2024-03-14 18:29:18 +0530

    Add files via upload

---
+++ forms/contact.js
@@ -0,0 +1,26 @@
+// forms/contact.js
+
+const { parse } = require('url');
+const { exec } = require('child_process');
+
+module.exports = async (req, res) => {
+  const { pathname, query } = parse(req.url, true);
+  
+  if (pathname === '/forms/contact.php') {
+    // Run the PHP script using the child_process module
+    exec('php forms/contact.php', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(stdout);
+    });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+};
